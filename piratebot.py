05232cdd06b1ccaf31c4695984cba4fb92fcaa12
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2019-11-12 22:08:23 -0500

    fix rate limit wait

--- piratebot.py
+++ piratebot.py
@@ -30,7 +30,7 @@ def get_api():
 
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_secret)
-	api = tweepy.API(auth)
+	api = tweepy.API(auth, wait_on_rate_limit=True)
 	return api
 
 
